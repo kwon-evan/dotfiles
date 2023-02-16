8da1f1c9acde9517dac743ac78881aba8cf92b00
kwon-evan <koun012@naver.com>
2023-02-16 16:46:35 +0900

    sumneko to lua_ls

--- nvim.packer/lua/kwon-evan/lsp/handlers.lua
+++ nvim.packer/lua/kwon-evan/lsp/handlers.lua
@@ -73,7 +73,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "sumneko_lua" then
+	if client.name == "lua_ls" then
 		client.server_capabilities.documentFormattingProvider = false
 	end
 

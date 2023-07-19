0cb495958cdc7b05da614e0f7ef54104c8550528
heonjin <koun012@naver.com>
2023-07-19 15:07:46 +0900

    refactor: rm useless things

--- nvim.packer/init.lua
+++
@@ -1,5 +0,0 @@
-require "kwon-evan.options"
-require "kwon-evan.keymaps"
-require "kwon-evan.plugins"
-require "kwon-evan.colorscheme"
-require "kwon-evan.lsp"
--- nvim.packer/lua/kwon-evan/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "kwon-evan.lsp.mason"
-require("kwon-evan.lsp.handlers").setup()
-require "kwon-evan.lsp.null-ls"

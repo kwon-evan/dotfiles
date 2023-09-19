05f557f2a9ea358a1d995fac6a5b4d6b42314abb
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-09-19 18:03:49 +0900

    change color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
@@ -6,12 +6,14 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.font = wezterm.font_with_fallback {
-  'Hack Nerd Font',
-  '나눔스퀘어'
-}
+config.font = wezterm.font_with_fallback({
+  "Hack Nerd Font",
+  "나눔스퀘어",
+})
 
-config.color_scheme = 'Tokyo Night (Gogh)'
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+config.color_scheme = 'Material Darker (base16)'
 config.window_background_opacity = 0.97
 
 return config

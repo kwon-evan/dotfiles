ddc3cab09cca2e1ce69275cf87ace93d4cec09e4
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-12-08 14:00:59 +0900

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,35 @@
 return {
+  -- {
+  --   "Mofiqul/dracula.nvim",
+  --   lazy = false,
+  --   name = "dracula",
+  --   priority = 1000,
+  --   config = function()
+  --     require("dracula").setup({
+  --       colors = {
+  --         bg = "#2b2b2b",
+  --       },
+  --       transparent_bg = true,
+  --     })
+  --     vim.cmd([[colorscheme dracula]])
+  --   end,
+  -- },
   {
-    "Mofiqul/dracula.nvim",
-    lazy = false,
-    name = "dracula",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      require("dracula").setup({
-        colors = {
-          bg = "#2b2b2b",
-        },
-        transparent_bg = true,
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        intergrations = {
+          headlines = true,
+          mason = true,
+          fidget = true,
+        }
       })
-      vim.cmd([[colorscheme dracula]])
-    end,
-  },
+
+      vim.cmd([[colorscheme catppuccin]])
+    end
+  }
 }

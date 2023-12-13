b7f829045d59eb15690fd65a0d1f04a4b4cb7aab
kwon-evan <koun012@naver.com>
2023-12-13 10:06:19 +0900

    back to dracula..

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,17 @@
 return {
-  -- {
-  --   "Mofiqul/dracula.nvim",
-  --   lazy = false,
-  --   name = "dracula",
-  --   priority = 1000,
-  --   config = function()
-  --     require("dracula").setup({
-  --       colors = {
-  --         bg = "#2b2b2b",
-  --       },
-  --       transparent_bg = true,
-  --     })
-  --     vim.cmd([[colorscheme dracula]])
-  --   end,
-  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    name = "dracula",
     priority = 1000,
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-        intergrations = {
-          headlines = true,
-          mason = true,
-          fidget = true,
-        }
+      require("dracula").setup({
+        colors = {
+          bg = "#212121",
+        },
+        transparent_bg = true,
       })
-
-      vim.cmd([[colorscheme catppuccin]])
-    end
-  }
+      vim.cmd([[colorscheme dracula]])
+    end,
+  },
 }

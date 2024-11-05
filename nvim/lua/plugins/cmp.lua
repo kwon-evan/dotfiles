e6affdecf66dd3858f587ff93bded7ae415ad28a
heonjin <koun012@naver.com>
2024-11-05 14:11:36 +0900

    fix keymap

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -40,7 +40,7 @@ return {
           side_padding = 0,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-g>"] = cmp.mapping.complete(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
